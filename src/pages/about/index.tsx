ab1ba1bb67064d9e12bfb88b3b555f497eb6f886
Renan Souza de Freitas <rnn.frts@gmail.com>
2023-01-26 18:31:57 -0300

    created about placeholder page

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Container } from './styles'
+
+import Layout from '../../components/Layout'
+
+const About: React.FC = () => {
+    return (
+        <Layout>
+            <Container>
+                <h1>Studio Dois Pontos</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </Container>
+            <Container>
+                <h1>Studio Dois Pontos</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </Container>
+        </Layout>
+    )
+}
+
+export default About
---
+++ src/pages/about/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const Container = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    h1, h2{
+        font-size: 32px;
+    }
+
+    p{
+        font-size: 16px;
+    }
+`
