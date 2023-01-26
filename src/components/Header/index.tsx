cf43c536a42a20973627cab0e62168651391f887
Renan Souza de Freitas <rnn.frts@gmail.com>
2023-01-26 18:32:23 -0300

    created placeholder menu functionality

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-import { Container } from './styles';
+import { Container, MenuButton, MenuContainer } from './styles';
 
 const Header: React.FC = () => {
+  const [visibility, setVisibility] = useState(false)
+
   return(
     <Container>
       <Link to="/">
         <h1>Dois Pontos</h1>
       </Link>
+
+      <MenuButton onClick={() => setVisibility(!visibility)}>Menu</MenuButton>
+
+      <MenuContainer isVisible={visibility}>
+        <Link to="/">Principal</Link>
+        <Link to="/sobre">Sobre</Link>
+        <Link to="/contato">Contato</Link>
+        <MenuButton onClick={() => setVisibility(!visibility)}>Close</MenuButton>
+      </MenuContainer>
     </Container>
   )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,9 +1,9 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const Container = styled.nav`
   position: fixed;
   z-index: 9;
-  background: #fff;
+  background: transparent;
   width: 100%;
   height: 80px;
   display: flex;
@@ -17,3 +17,32 @@ export const Container = styled.nav`
     color: #707070;
   }
 `;
+
+export const MenuButton = styled.button`
+  font-weight: bold;
+  font-size: 26px;
+`
+
+interface IContainerProps {
+  isVisible: boolean;
+}
+
+export const MenuContainer = styled.div<IContainerProps>`
+  display: none;
+
+  ${props => props.isVisible && css`
+    display: flex;
+  `}
+
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  background: rgba(255, 255, 255, .7);
+`
