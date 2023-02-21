8f62ca2998dcfbccbb250ab517451f06ea930880
Renan Souza de Freitas <rnn.frts@gmail.com>
2023-02-21 18:50:37 -0300

    using data from loader

--- src/api.ts
+++ src/api.ts
@@ -1,9 +1,5 @@
-import {createClient, Asset, Entry} from 'contentful'
-
-const client = createClient({
-  space: process.env.REACT_APP_CONTENTFUL_SPACE_ID || '',
-  accessToken: process.env.REACT_APP_CONTENTFUL_PRODUCTION_TOKEN || ''
-})
+import {createClient, Asset, ContentfulClientApi} from 'contentful'
+import { redirect } from 'react-router-dom'
 
 export interface IProjectFields {
   animations: Asset[];
@@ -19,12 +15,35 @@ export interface IProject {
   }
 }
 
+const getContentfulClient = () => {
+  let client: ContentfulClientApi
+  client = {} as ContentfulClientApi
+  try {
+    client = createClient({
+      space: process.env.REACT_APP_CONTENTFUL_SPACE_ID || '',
+      accessToken: process.env.REACT_APP_CONTENTFUL_PRODUCTION_TOKEN || ''
+    })
+  } catch {
+    navigateToNotFound()
+  }
+
+  return client
+}
+
+const navigateToNotFound = () => redirect('/not-found')
+
 export const loadProjects = async () => {
-  const response = await client.getEntries<IProjectFields>()
-  return response.items
+  const client = getContentfulClient()
+  try {
+    const response = await client.getEntries<IProjectFields>()
+    return response.items
+  } catch {
+    return redirect('/not-found')
+  }
 }
 
 export const loadProject = async (id: string) => {
+  const client = getContentfulClient()
   const response = await client.getEntry<IProjectFields>(id)
   return response
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,36 +1,25 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import { loadProjects, IProject } from '../../api'
+import React from 'react'
+import { Link, useLoaderData } from 'react-router-dom'
 
+import { IProject } from '../../api'
 import { Container, Project, LinkText } from './styles'
-import Layout from '../../components/Layout'
 
 const Home: React.FC = () => {
-  const [projects, setProjects] = useState<IProject[]>()
-
-  const load = async () => {
-    const loadedProjects = await loadProjects()
-    setProjects(loadedProjects)
-  }
-
-  useEffect(() => {
-    load()
-  }, []);
+  const loaderData: any = useLoaderData()
+  const projects: IProject[] = loaderData
 
   return (
-    <Layout>
-      <Container>
-        {projects && projects.map(project => (
-          <Project key={project.sys.id} bgUrl={project.fields.animations[0].fields.file.url}>
-            <Link to={`/projeto/${project.sys.id}`}>
-              <LinkText>
-                {project.fields.title}
-              </LinkText>
-            </Link>
-          </Project>
-        ))}
-      </Container>
-    </Layout>
+    <Container>
+      {projects && projects.map(project => (
+        <Project key={project.sys.id} bgUrl={project.fields.animations[0].fields.file.url}>
+          <Link to={`/projeto/${project.sys.id}`}>
+            <LinkText>
+              {project.fields.title}
+            </LinkText>
+          </Link>
+        </Project>
+      ))}
+    </Container>
   );
 }
 
