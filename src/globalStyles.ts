ba182fdfa8ebf800e30913d8b9262c73547e192f
Renan Souza de Freitas <rnn.frts@gmail.com>
2023-03-15 13:40:37 -0300

    [feat] import and cofigure material symbols

--- src/globalStyles.ts
+++ src/globalStyles.ts
@@ -9,6 +9,14 @@ const GlobalStyle = createGlobalStyle`
     text-decoration: none;
     font-family: 'Quicksand', sans-serif;
   }
+  
+  .material-symbols-outlined {
+    font-variation-settings:
+    'FILL' 0,
+    'wght' 900,
+    'GRAD' 0,
+    'opsz' 48
+  }
 `;
 
 export default GlobalStyle;
