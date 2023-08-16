b7993cdacef0fe4438132a55ba8218dbd1c60f06
renansdf <rnn.frts@gmail.com>
2023-08-16 19:39:58 +0100

    refactor: fix navigation and add projects covers

--- src/pages/project/index.tsx
+++ src/pages/project/index.tsx
@@ -1,13 +1,14 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
-import { type IProject, loadProject, navigateToNotFound } from '../../utils/Api';
+import { type IProject, loadProject } from '../../utils/Api';
 
 import { ProjectsContainer, Title, Image } from './styles';
 
 const Project: React.FC = () => {
-  const params = useParams();
-  const [project, setProject] = useState<IProject>();
+  const navigate = useNavigate()
+  const params = useParams()
+  const [project, setProject] = useState<IProject>()
 
   const load = useCallback(async () => {
     if(params.projectid != null){
@@ -17,13 +18,21 @@ const Project: React.FC = () => {
   },[params.projectid])
 
   useEffect(() => {
-    load().catch(() => navigateToNotFound())
+    load().catch(() => {navigate('/not-found')})
   }, [load]);
 
   return (
     <ProjectsContainer itemsAmount={2}>
       <Title position={1}>{project?.fields.title}</Title>
-      {project?.fields.animations.map((obj, index) => (
+      {project?.fields.coverFrames?.map((obj, index) => (
+        <Image 
+          key={obj.fields.file.url} 
+          src={obj.fields.file.url} 
+          alt={obj.fields.file.url} 
+          position={index+2}
+        />
+      ))}
+      {project?.fields.animations?.map((obj, index) => (
         <Image 
           key={obj.fields.file.url} 
           src={obj.fields.file.url} 
