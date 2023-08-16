bc50182e180b89aa0b69b738e5269f4b81148ec7
renansdf <rnn.frts@gmail.com>
2023-08-16 19:39:23 +0100

    refactor: filter projects without content

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,8 +5,17 @@ import { type IProject } from '../../utils/Api'
 import { Container, Project, LinkText } from './styles'
 
 const Home: React.FC = () => {
-  const projects = useLoaderData() as IProject[]
+  const loadedProjects = useLoaderData() as IProject[]
+  const [projects, setProjects] = useState<IProject[]>([] as IProject[])
   const [frameToggle, setFrameToggle] = useState(true)
+
+  useEffect(() => {
+    const projectsList: IProject[] = []
+    loadedProjects.forEach((project) => {
+      if(project.fields.coverFrames != null) projectsList.push(project)
+    })
+    setProjects(projectsList)
+  }, [])
 
   useEffect(() => {
     const togglerInterval = setInterval(() => {
@@ -15,11 +24,11 @@ const Home: React.FC = () => {
 
     return () => { clearInterval(togglerInterval) }
   }, [])
-
+  
   return (
     <Container>
       {projects?.map(project => (
-        <Project key={project.sys.id} frameStep={frameToggle} animationFrames={project.fields.animations}>
+        <Project key={project.sys.id} frameStep={frameToggle} animationFrames={project.fields.coverFrames}>
           <Link to={`/projeto/${project.sys.id}`}>
             <LinkText>
               {project.fields.title}
