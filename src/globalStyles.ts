34cc4716c6a7fa1c7a9ddf47fce45f0891a3748d
renansdf <rnn.frts@gmail.com>
2023-12-06 19:50:26 +0000

    feat: update font configuration

--- src/globalStyles.ts
+++ src/globalStyles.ts
@@ -1,46 +1,13 @@
 import { createGlobalStyle } from "styled-components";
 
-import CarbonaLight from './fonts/Carbona-Light.otf';
-import CarbonaRegular from './fonts/Carbona-Regular.otf';
-import CarbonaBold from './fonts/Carbona-Bold.otf';
-import CarbonaBlack from './fonts/Carbona-Black.otf';
-
 const GlobalStyle = createGlobalStyle`
-  @font-face {
-    font-family: 'Carbona';
-    src: url(${CarbonaLight}) format('opentype');
-    font-weight: light;
-    font-style: normal;
-  }
-
-  @font-face {
-    font-family: 'Carbona';
-    src: url(${CarbonaRegular}) format('opentype');
-    font-weight: normal;
-    font-style: normal;
-  }
-
-  @font-face {
-    font-family: 'Carbona';
-    src: url(${CarbonaBold}) format('opentype');
-    font-weight: bold;
-    font-style: normal;
-  }
-
-  @font-face {
-    font-family: 'Carbona';
-    src: url(${CarbonaBlack}) format('opentype');
-    font-weight: black;
-    font-style: normal;
-  }
-
   html, body, *{
     margin: 0;
     box-sizing: border-box;
     padding: 0;
     list-style: none;
     text-decoration: none;
-    font-family: 'Carbona', sans-serif;
+    font-family: 'Space Grotesk', sans-serif;
   }
   
   .material-symbols-outlined {
@@ -53,7 +20,7 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 export const defaultValues = {
-  padding: '60px'
+  padding: '10%'
 }
 
 export default GlobalStyle;
